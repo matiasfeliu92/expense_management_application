2758f82d74f03b3eb146d508911407a97138eb0f
matiasfeliu92 <matumazparrote@gmail.com>
2024-10-25 12:55:50 -0300

    add config for connect to mysql local database

---
+++ src/db/config.py
@@ -0,0 +1,20 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db_url = os.getenv('DB_URL')
+print({'DB_URL': db_url})
+engine = create_engine(db_url)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
