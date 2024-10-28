43e5fabba76404f111d8640b9ae4d34bee7fcc9b
matiasfeliu92 <matumazparrote@gmail.com>
2024-10-28 17:14:20 -0300

    update operation_service by validating operation type and adding their category

--- src/services/operation_service.py
+++ src/services/operation_service.py
@@ -1,10 +1,10 @@
 from sqlalchemy.orm import Session
-from sqlalchemy import and_
+from sqlalchemy import and_, or_
 from fastapi import HTTPException, status
 from fastapi.responses import JSONResponse
 
 from ..core import Security
-from ..models import Operation, OperationType
+from ..models import Operation, OperationType, User, Category
 from ..schemas import CreateOperation
 
 class OperationService():
@@ -25,7 +25,16 @@ class OperationService():
         operation.pop('_sa_instance_state', None)
         return JSONResponse(content=operation_dict, status_code=status.HTTP_200_OK)
     
-    def create_new(self, user_id: int,  operation: CreateOperation):
+    def create_new(self, user_id: int, operation: CreateOperation):
+        new_balance = None
+        print(operation.to_dict())
+        category = self.db.query(Category).filter(Category.name.ilike(f'%{operation.category}%')).first()
+        print(category)
+        if not category:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=f"The category with id {operation.category_id} does not exist."
+            )
         print({'operation_from_form_in_service': operation.to_dict})
         if operation.type.lower() == 'egreso':
             operation.type = OperationType.expense
@@ -33,9 +42,16 @@ class OperationService():
             operation.type = OperationType.income
         else:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Not valid operation type, you must enter {[member.value for member in OperationType]} or {[member.value.lower() for member in OperationType]}")
-        new_operation = Operation(concept=operation.concept, amount=operation.amount, type=operation.type, user_id=operation.user_id)
+        new_operation = Operation(concept=operation.concept, amount=operation.amount, type=operation.type, user_id=operation.user_id, category_id=category.id)
         print({'new_operation': new_operation})
+        user = self.db.query(User).filter(User.id == user_id).first()
+        if operation.type == 'Expense' or operation.type == 'expense':
+            new_balance = user.balance - operation.amount
+        elif operation.type == 'Income' or operation.type == 'income':
+            new_balance = user.balance + operation.amount
+        print({'new_balance': new_balance})
         self.db.add(new_operation)
+        user.balance = new_balance
         self.db.commit()
         self.db.refresh(new_operation)
         new_operation_dict = new_operation.__dict__.copy()
