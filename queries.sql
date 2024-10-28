f29c2831120368799f1fb59855e4c85b6b5d7625
matiasfeliu92 <matumazparrote@gmail.com>
2024-10-28 17:26:38 -0300

    add insert into for set default categories

--- queries.sql
+++ queries.sql
@@ -13,3 +13,11 @@ INNER JOIN
     expenses 
 ON 
     users.id = expenses.user_id;
+
+
+INSERT INTO categories (name, description)
+VALUES ("salary", "This category covers the collection of salaries for jobs"), 
+("service payment", "This category covers the payment of utility bills such as electricity, gas, telephone, internet, water, etc."), 
+("shopping", "This category covers purchases made, whether food, clothing, appliances, household items, furniture, etc."), 
+("rent", "This category covers the payment of rent for housing, whether apartments or houses."),
+("expense", "This category covers the payment of real estate expenses in the buildings/houses where one lives.")
