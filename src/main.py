72e9f5411b749639fb254f73555e05b143ed6f3b
matiasfeliu92 <matumazparrote@gmail.com>
2024-10-28 17:23:55 -0300

    add Category model import

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from .db.config import Base, engine, SessionLocal
 from sqlalchemy.orm import Session
-from .models import User, Operation
+from .models import User, Operation, Category
 from .api import UserRoutes, OperationRoutes
 from .middlewares import LoadUserData
 
